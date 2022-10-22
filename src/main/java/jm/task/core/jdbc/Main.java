5b6bd0c1e1f8b258a2ed5ab2c3278be888f154c3
AlKichen <kich7777777@gmail.com>
2022-10-22 20:30:26 +0300

    init commit check

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,5 +3,7 @@ package jm.task.core.jdbc;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        //"jdbc:mysql://localhost:3306/mydbtest"
+        //проверка
     }
 }
