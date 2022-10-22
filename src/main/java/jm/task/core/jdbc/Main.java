3b98ccc4cb9d20080dc1021391e82f38f0353d67
AlKichen <kich7777777@gmail.com>
2022-10-22 22:06:29 +0300

    add method getConnection in Util class

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,13 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         //"jdbc:mysql://localhost:3306/mydbtest"
         //проверка
+        Util util = new Util();
+        util.getConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+//import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private final String USERNAME = "root1";
+    private final String PASSWORD = "root1";
+
+    public Connection getConnection() {
+        Connection connection;
+
+        try {
+            /*Driver driver = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver(driver);*/
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Connection OK");
+            } else {
+                System.out.println("Connection Error!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
