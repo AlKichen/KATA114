94224391ec5331c058842d76e79771e3a3ed5c47
AlKichen <kich7777777@gmail.com>
2022-10-24 23:52:51 +0300

    fix Util class on Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,16 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserServiceImpl us = new UserServiceImpl();
+
+
+
+        /*UserServiceImpl us = new UserServiceImpl();
         us.createUsersTable();
         us.saveUser("имя", "фамилия", (byte) 26);
         us.saveUser("имя2", "2фамилия", (byte) 66);
@@ -16,7 +20,7 @@ public class Main {
         us.removeUserById(3);
         us.cleanUsersTable();
         System.out.println(us.getAllUsers());
-        us.dropUsersTable();
+        us.dropUsersTable();*/
 
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,55 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
+import com.mysql.cj.xdevapi.SessionFactory;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+/*import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.SQLException;*/
+import java.util.Properties;
+
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null){
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest");
+                properties.put(Environment.USER, "root1");
+                properties.put(Environment.PASS, "root1");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = (SessionFactory) configuration.buildSessionFactory(serviceRegistry);
+            } catch (HibernateException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sessionFactory;
+    }
+
+
+
+
+    /*private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USERNAME = "root1";
     private static final String PASSWORD = "root1";
 
@@ -23,5 +65,5 @@ public class Util {
             throw new RuntimeException(e);
         }
         return connection;
-    }
+    }*/
 }
